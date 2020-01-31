ec5f4e43103e662fa1bc5d71f8012e5f90d951e3
Ignacio Cano <i.cano@gbh.com.do>
2020-01-31 19:20:54 -0400

    adds size options and default option

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,13 @@
 const test = require("tape");
 const lib = require("../lib");
 
-test("It creates a gravat url for a given email", t => {
-  t.plan(1);
+test("It creates gravatar urls with the given options", t => {
+  t.plan(4);
   t.equal(
     lib("ignacioj.cano@gmail.com"),
     "https://www.gravatar.com/avatar/a4d86421317a533a4b8dcdeda3e5ebd8"
   );
+  t.equal(lib("ignacioj.cano@gmail.com", {size: 200}), "https://www.gravatar.com/avatar/a4d86421317a533a4b8dcdeda3e5ebd8?s=200");
+  t.equal(lib("ignacioj.cano@gmail.com", {default: 'monsterid'}), "https://www.gravatar.com/avatar/a4d86421317a533a4b8dcdeda3e5ebd8?d=monsterid")
+  t.equal(lib("ignacioj.cano@gmail.com", {default: 'monsterid', size: 200}),"https://www.gravatar.com/avatar/a4d86421317a533a4b8dcdeda3e5ebd8?s=200&d=monsterid")
 });
