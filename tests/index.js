6ab03dbc28b0cc7bba1f2be5839bb2f4925eb38d
Ignacio Cano <i.cano@gbh.com.do>
2020-01-31 18:50:47 -0400

    adds an actual implementation

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,10 @@
 const test = require("tape");
 const lib = require("../lib");
 
-test("It does something", t => {
+test("It creates a gravat url for a given email", t => {
   t.plan(1);
-  t.equal(lib(), "hi");
+  t.equal(
+    lib("ignacioj.cano@gmail.com"),
+    "https://www.gravatar.com/avatar/a4d86421317a533a4b8dcdeda3e5ebd8"
+  );
 });
